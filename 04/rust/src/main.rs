6b6c603e2136eefe9047cf0ec081c9af7b9a31a9
mhepburn <mhepburn@podfather.com>
2022-12-04 09:26:06 +0000

    Day 4 part a

---
+++ 04/rust/src/main.rs
@@ -0,0 +1,95 @@
+use std::env;
+use std::fs;
+use core::str::Lines;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let input_filename = &(&args[2])[..];
+
+    let result = match &(&args[1])[..] {
+        "--solve-a" => solve_a(input_filename),
+        "--solve-b" => solve_b(input_filename),
+        _ => panic!("Must provide --solve-a or --solve-b as first arg")
+    };
+
+    match result {
+        Ok(val) => println!("{}", val),
+        Err(e) => panic!("{}", e)
+    }
+}
+
+type ResultOrErr<X> = Result<X, String>;
+struct ElfAssignment {
+    lower: i32,
+    upper: i32
+}
+
+impl ElfAssignment {
+    fn fully_contains(&self, other: &ElfAssignment) -> bool {
+        return self.lower <= other.lower && self.upper >= other.upper;
+    }
+
+    fn output(&self) {
+        println!("{}-{}", self.lower, self.upper);
+    }
+}
+
+struct ElfPair {
+    assignments: [ElfAssignment; 2]
+}
+
+fn parse_assignment(input: &str) -> ElfAssignment
+{
+    let parts: Vec<&str> = input.split('-').collect();
+    return ElfAssignment{
+        lower: parts[0].parse::<i32>().unwrap(),
+        upper: parts[1].parse::<i32>().unwrap(),
+    }
+}
+
+fn parse_input(input: Lines) -> Vec<ElfPair>
+{
+    let mut output: Vec<ElfPair> = Vec::new();
+    for line in input {
+        if line == "" {
+            continue
+        }
+        let assignment_strings : Vec<&str> = line.split(',').collect();
+        let elf_pair = ElfPair{assignments: [
+            parse_assignment(assignment_strings[0]), parse_assignment(assignment_strings[1])
+        ]};
+        output.push(elf_pair);
+    }
+
+    return output;
+}
+
+fn solve_a(input_filename: &str) -> ResultOrErr<i32> {
+    let input: String = load_input(input_filename)?;
+    let elf_pairs = parse_input(input.lines());
+
+    let mut fully_contained = 0;
+
+    for elf_pair in elf_pairs {
+        print!("{}", "Elf 1: ");
+        elf_pair.assignments[0].output();
+        if elf_pair.assignments[0].fully_contains(&elf_pair.assignments[1]) ||
+            elf_pair.assignments[1].fully_contains(&elf_pair.assignments[0]) {
+            fully_contained += 1;
+        }
+    }
+
+    return Ok(fully_contained);
+}
+
+fn solve_b(_input_filename: &str) -> ResultOrErr<i32> {
+    return Err("Not implemented".to_string())
+}
+
+fn load_input(input_filename: &str) -> ResultOrErr<String> {
+    return match fs::read_to_string(input_filename) {
+        Ok(x) => Ok(x),
+        Err(x) => Err(x.to_string())
+    };
+}
